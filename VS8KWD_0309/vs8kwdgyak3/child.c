122c8917aa2ab99d38bb4a736e089f48739cb052
Gadaniel <79148329+Gadaniel@users.noreply.github.com>
2021-03-11 23:23:14 +0100

    Add files via upload

---
+++ VS8KWD_0309/vs8kwdgyak3/child.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    int x;
+    for(x = 0; x < 5; x++){
+        printf("Gancsos Daniel VS8KWD\n");
+    }
+    return 0;
+}
+
